1a3968d1db90f44634f022473a924b1c0ef0c5d0
muskan <muskantuteja714@gmail.com>
2026-04-22 15:08:46 +0530

    adv java

---
+++ advancedjava/first-hibernate/src/main/java/com/jsp/A.java
@@ -0,0 +1,15 @@
+package com.jsp;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class A {
+	
+	@Id
+	private int id;
+	private String name;
+	private String Email;
+	private int marks;
+	
+
+}
