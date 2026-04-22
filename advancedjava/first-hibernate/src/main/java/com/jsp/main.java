565829212b62c8545d8647fbb960eafcd9b81bbb
muskan <muskantuteja714@gmail.com>
2026-04-22 16:17:14 +0530

    adv java

--- advancedjava/first-hibernate/src/main/java/com/jsp/main.java
+++ advancedjava/first-hibernate/src/main/java/com/jsp/main.java
@@ -1,5 +1,20 @@
 package com.jsp;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class main {
+public static void main(String[] args) {
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("java");
+	EntityManager em = emf.createEntityManager();
+	EntityTransaction et = em.getTransaction();
+	
+	Students s1 = new Students (1,"anhjj",45,"n@234");
+	em.persist(s1);
+	et.commit();
+	
+}
 
 }
