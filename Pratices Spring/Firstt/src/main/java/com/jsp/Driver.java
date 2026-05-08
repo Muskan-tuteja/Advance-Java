7bb3b26ce69ed5c86ce0cf016b9270a25d5f5ee5
muskan <muskantuteja714@gmail.com>
2026-05-08 15:56:10 +0530

    spring

--- Pratices Spring/Firstt/src/main/java/com/jsp/Driver.java
+++ Pratices Spring/Firstt/src/main/java/com/jsp/Driver.java
@@ -2,17 +2,30 @@ package com.jsp;
 
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("hello world");
+//        System.out.println("hello world");
 
 
         //spring container
-        ApplicationContext context
+       ConfigurableApplicationContext  context
                 = new ClassPathXmlApplicationContext("config.xml");
-        
+//        Object ob = context.getBean("Students");
+        Person person = (Person) context.getBean("person");
+        Person s=(Person) person;
+        System.out.println(s.getName());
+        System.out.println(s.getAge());
+        System.out.println(s.getId());
+//        Students s = (Students) ob;
+//        s.m1();
+
+
+//        System.out.println(context.isActive());
+//        context.close();
+//        System.out.println(context.isActive());
 
 
 
