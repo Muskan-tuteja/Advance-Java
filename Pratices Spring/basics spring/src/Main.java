6912fdd7cc9fb8a6742f3941e7c2f284def602fa
muskan <muskantuteja714@gmail.com>
2026-05-14 22:46:24 +0530

    spring boot

--- Pratices Spring/basics spring/src/Main.java
+++ Pratices Spring/basics spring/src/Main.java
@@ -11,14 +11,22 @@ UserServices userServices = new UserServices();
 userServices.notifyUser("Hello order placesd!");
 
 
-//loose
+//loose email interface
 NotificationServices emailServices = new EmailNotificationServices();
 lossecoupling.UserServices userServices2 = new lossecoupling.UserServices(emailServices);
 userServices2.notifyUser("Hello order placesd!");
-
+// losse sms interfaces
         NotificationServices smsServices = new SmsNotificationServices();
         lossecoupling.UserServices userServices3 = new lossecoupling.UserServices(smsServices);
     userServices3.notifyUser("Hello order placesd!");
+
+   // loose setterinjection
+
+        lossecoupling.UserServices userServicesSetter = new lossecoupling.UserServices();
+        userServicesSetter.setNotification(emailServices);
+
+        // loose field injection
+//        userServicesSetter.getNotification=emailServices;
     }
 
 
--- Pratices Spring/basics spring/src/lossecoupling/UserServices.java
+++ Pratices Spring/basics spring/src/lossecoupling/UserServices.java
@@ -5,11 +5,23 @@ package lossecoupling;
 public class UserServices {
     NotificationServices notification ;
 
+    public UserServices() {
+    }
+
     public UserServices(NotificationServices notification) {
+        this.notification = notification;
+    }
+
+    public NotificationServices getNotification() {
+        return notification;
+    }
+
+    public void setNotification(NotificationServices notification) {
         this.notification = notification;
     }
 
     public void notifyUser(String message){
     notification.send("Hello World");
+
 }
 }
