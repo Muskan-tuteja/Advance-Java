bde673289bd793ec5860263af39a035b34ab84fd
muskan <muskantuteja714@gmail.com>
2026-05-16 22:23:23 +0530

    spring boot

--- Pratices Spring/Spring-boot/src/main/java/com/jsp/spring/HelloController.java
+++ Pratices Spring/Spring-boot/src/main/java/com/jsp/spring/HelloController.java
@@ -10,4 +10,12 @@ public class HelloController {
     public String sayHello(){
         return "Hello World!hii ";
     }
+
+    @GetMapping("/user")
+    public User getUser(){
+        User user = new User(1,"muskkan",34);
+        User user2 = new User(2,"musu",31);
+        return user2;
+
+    }
 }
