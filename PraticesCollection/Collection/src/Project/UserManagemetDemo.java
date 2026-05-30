b8c643b4d0947000bdb1150cc941599af2780319
muskan <muskantuteja714@gmail.com>
2026-05-30 22:33:46 +0530

    StartProject

--- PraticesCollection/Collection/src/Project/UserManagemetDemo.java
+++ PraticesCollection/Collection/src/Project/UserManagemetDemo.java
@@ -2,9 +2,11 @@ package Project;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class UserManagemetDemo {
@@ -18,6 +20,7 @@ public class UserManagemetDemo {
 		users.add(new User("mahi",false,new HashSet<>(Arrays.asList("USER"))));
 		
 		users.add(new User("mahi123",true,new HashSet<>(Arrays.asList("MANAGER"))));
+		users.add(new User("mahiii",true,new HashSet<>(Arrays.asList("MANAGER"))));
 	
 //	Remove inactive users
 		
@@ -26,10 +29,25 @@ public class UserManagemetDemo {
 			if(!iterator.next().isActive())
 				iterator.remove();
 		}
+//		print active users
 		System.out.println("ACTIVE USER");
-		
 		for(User user : users)
 			System.out.println(user.getName());
+		
+		//Count user per role
+		Map<String,Integer> roleCount = new HashMap();
+		for(User user : users) {
+			for (String role : user.getRoles()) {
+				roleCount.put(role, roleCount.getOrDefault(role, 0)+1);
+			}
+		}
+		System.out.println(".....All Users : Count .......");
+		for(Map.Entry<String,Integer> entry : roleCount.entrySet() ) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+			
+		}
+		
+		
 	}
 
 }
