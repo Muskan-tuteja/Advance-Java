350397fa173825dbfad7b9b2e3d215adfcea017c
muskan <muskantuteja714@gmail.com>
2026-05-30 21:36:58 +0530

    Collection

---
+++ PraticesCollection/Collection/src/MapProgram/MapDemo.java
@@ -0,0 +1,17 @@
+package MapProgram;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo {
+	public static void main(String[] args) {
+		Map <Integer,String> users = new HashMap<Integer, String>();
+		users.put(1, "Muskan");
+		users.put(2, "Mahi");
+		users.put(3, "Hashi");
+		
+		System.out.println(users);
+		System.out.println(users.get(2));
+	}
+
+}
